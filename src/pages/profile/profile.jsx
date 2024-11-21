3a57fee906380508ec06aa1db3a6894762731101
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2024-11-21 09:47:55 -0300

    1.0

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 const profile = () => {
   return (
     <>
-      
+        <h2>Perfil</h2>
+            <p>Nome: {name}</p>
+            <input
+              type="Rua"
+              name="Rua"
+              id="Rua"
+              placeholder="Rua"
+              className="Rua"
+            />
+            <input
+              type="Numero"
+              name="Numero"
+              id="Numero"
+              placeholder="Numero"
+              className="Numero"
+            />
+            <input
+              type="Complemento"
+              name="Complemento"
+              id="Complemento"
+              placeholder="Complemento"
+              className="Complemento"
+            />
     </>
   )
 }
