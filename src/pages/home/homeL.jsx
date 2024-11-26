76b8b38666040011388301eb25a06e1e3e02092a
NICOLAS PIMENTEL FERNANDES <nicolas_fernandes@estudante.sesisenai.org.br>
2024-11-26 09:52:34 -0300

    adotei um gato

--- src/pages/home/homeL.jsx
+++ src/pages/home/homeL.jsx
@@ -36,13 +36,16 @@ const homeL = () => {
             <img src="https://s3-alpha-sig.figma.com/img/c6d6/0221/8c59a8ba3353a35e54e15b2200c50088?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Z-ykFzDTX9jfPkw79Ei~avFZp6H9eCXdXtiJRTkPNWuEBQiayltu8B1rL3HvAsZ8R1QvnTbwk3EhWlRQfDxijkMiL47Mdg3Vt6Utbu4enIm7~e~~uukH1DSknYEgx7CECo~2WVCz1M-CBS54Z64cT0K2pIV1-1Q21qW-DuekrIWJ1agS5hiRFpFzAYjEAXsfm4HreoJOCu3eAC~oHK5AajGkFx9WMg6ulvjgT4Y4vgpK~mMfueKwCO~lU0bmlToRgU6BA7zJE2y-I9FpP78HFyIy62DGhMmm1OJ3gpBzhI5fGNf1a8Xh~S8ahXoZ1Yf1kTLV~Pj-nAqVCTGBvTYWFw__" alt="" />
             <div className='aaaa'>
               <div>
-
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/8b2b/b269/3b5e68f0db64622933965ab87d7ca17d?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SJuesD4zmm316MJRc6gTdpRQvggSSuAiIzFE~AmKDOIKsSt~CRRBy0JTajINMzQbjizFZzra5s~thh3r10J9-jG6fqB77UcLkLZbGJgIfquqANS8iQMGSrXSHFmX4AT34xAMqLIcobSsNEoH-~6bbICZp85c2GQSeeWc0DQH13e4gA-J1o27H8pQx7wb2rVQ0pUYvjXe6vqXvDjmhq9rUbEXUYDpLQmykhOQAaH2nMNYKSScSrvyMEwfP0qvL-QGAKzaSzTkJ1ows-HecoDQJ~NbC7-mfqpY-a0SkpJbvC958IMlEGsmYXFNX9UN3iHzZUEGzVXAKsE7CJr-BgWcrQ__" alt="" />
+                <span>42</span>
               </div>
               <div>
-
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/63af/be91/280e317dcff5a81a9fb9d5cc27d76db3?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MxQw-z2bnuJSR2vAofxxSgoqsMUl~zOP4gSDbpT-imOGrk8PPffg1imf2rXDIZA8cPhAy~xRoO8POtjhpi0OtTGzUBEhgHXcKOKAHYFOFWSIpsF19SukoB9cHnm0Zyoe5LbxhbNrR-28db7L3HQTxVPRsQE~d-EYLZxyyQ6nDQICABoZv0RTqIxOWA~Y1zir4w8jJ7zeb0v~0pfnloAX0378R2qT05C67ZOPSZ2AH11qGc3rmyF24UV1oW2SMZBE9Ngp3zMDP221lUDD9Rhrc3~y88naIQJkFwCqiA6lYgUpfFFoqesVIc59njAxTeyhsGIQwH5kU7ltR-TddonY1g__" alt="" />
+                <span>102</span>
               </div>
               <div>
-                
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/5153/3966/d0b7d0870c425b015fedc9ae4e8b0920?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KWYIVxLXykvknvHVHZFu~2MPLivH833RxYaa13lroXqLalGGRAxsKqF1gUktQA-sOqQWUH~YwPcpRz-OREBxDjisio7JOL0EkfS~RnNOKd4JHMz98kLy0LZW1ToeIs0UyUODBpiK~rK34JRyHDIQZsZtP56FnJ1GppHRPmkriQ9kvMhxtngbhWecogLMQl5rzMjf8mFB6njTw6AnaXT1f1McJWK4YxOGninyllRNVuM2dWa405hu~YmRr8BCbgGuU2zByhcRCAUkjnEd6w1G5w5BM38kLqttFQtFLZJJPRl7riBlXR7D-U8CkSDKxQZWp-4vfDX5IcLJhenHIbmxhw__" alt="" />
+                <span>10</span>
               </div>
             </div>
           </div>
@@ -54,7 +57,45 @@ const homeL = () => {
             </div>
             <p>Procuro alguem para arrumar meu chuveiro.</p>
             <img src="https://s3-alpha-sig.figma.com/img/8e52/ee2d/3c4904404afb384857b4724eec96252e?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X57W7FobFkTbT-mK7tt0-VPPoDgNeLNtgmYncZmH-Isdplqr8hQghebgVk6cFdzdY7FSFjVV9BbEJDTbTbgCh2OCmJ7EGqcBWOgDi~4oV444stq4gfB9zze-odbrtf1B2FDYILsEJt3mfnveaWlgUD6HkQLH37XZcnVYRrOgQ5TN91Vk2inSEiMekMacvN~05ghRKRj-GfY543TjSiLD4r-oOKstbjua0P3ZAxAJdWzxQfjQwi6Ab91sU8Ln3nVDZ4YIsJcz-bf9IF7fkCA8FOHbb17RwHLnSxbB5dDbON9YpxVTlHhPIb6eKHwEOzdPQ50L2gFEnhRWuejOxXH~tQ__" alt="" />
+            <div className='aaaa'>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/8b2b/b269/3b5e68f0db64622933965ab87d7ca17d?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SJuesD4zmm316MJRc6gTdpRQvggSSuAiIzFE~AmKDOIKsSt~CRRBy0JTajINMzQbjizFZzra5s~thh3r10J9-jG6fqB77UcLkLZbGJgIfquqANS8iQMGSrXSHFmX4AT34xAMqLIcobSsNEoH-~6bbICZp85c2GQSeeWc0DQH13e4gA-J1o27H8pQx7wb2rVQ0pUYvjXe6vqXvDjmhq9rUbEXUYDpLQmykhOQAaH2nMNYKSScSrvyMEwfP0qvL-QGAKzaSzTkJ1ows-HecoDQJ~NbC7-mfqpY-a0SkpJbvC958IMlEGsmYXFNX9UN3iHzZUEGzVXAKsE7CJr-BgWcrQ__" alt="" />
+                <span>12</span>
+              </div>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/63af/be91/280e317dcff5a81a9fb9d5cc27d76db3?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MxQw-z2bnuJSR2vAofxxSgoqsMUl~zOP4gSDbpT-imOGrk8PPffg1imf2rXDIZA8cPhAy~xRoO8POtjhpi0OtTGzUBEhgHXcKOKAHYFOFWSIpsF19SukoB9cHnm0Zyoe5LbxhbNrR-28db7L3HQTxVPRsQE~d-EYLZxyyQ6nDQICABoZv0RTqIxOWA~Y1zir4w8jJ7zeb0v~0pfnloAX0378R2qT05C67ZOPSZ2AH11qGc3rmyF24UV1oW2SMZBE9Ngp3zMDP221lUDD9Rhrc3~y88naIQJkFwCqiA6lYgUpfFFoqesVIc59njAxTeyhsGIQwH5kU7ltR-TddonY1g__" alt="" />
+                <span>73</span>
+              </div>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/5153/3966/d0b7d0870c425b015fedc9ae4e8b0920?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KWYIVxLXykvknvHVHZFu~2MPLivH833RxYaa13lroXqLalGGRAxsKqF1gUktQA-sOqQWUH~YwPcpRz-OREBxDjisio7JOL0EkfS~RnNOKd4JHMz98kLy0LZW1ToeIs0UyUODBpiK~rK34JRyHDIQZsZtP56FnJ1GppHRPmkriQ9kvMhxtngbhWecogLMQl5rzMjf8mFB6njTw6AnaXT1f1McJWK4YxOGninyllRNVuM2dWa405hu~YmRr8BCbgGuU2zByhcRCAUkjnEd6w1G5w5BM38kLqttFQtFLZJJPRl7riBlXR7D-U8CkSDKxQZWp-4vfDX5IcLJhenHIbmxhw__" alt="" />
+                <span>3</span>
+              </div>
+            </div>
           </div>
+          <hr />
+          <div>
+            <div>
+              <img className='user' src="https://s3-alpha-sig.figma.com/img/4356/5f0d/f9f95680fdeab4047d9ee043abbed5fc?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P4e-AZCc0FxuS4wLi4eKzHfqmra-aQPQ9ujmPhSZftY0Cjqw9edcZluUto-3gP83wobfDiGPhnbDYRhNfA3IwLV~qBY~TfwjtbyMZ0aJ8GNgOHJIV5ePIsMb9o7YzOkAoOyHKKF6ZcxrjR0bE3VciO0~aCRtwKRlGPeFHb5AtsbL9hWMypvJ58f2a8kSTfSpVl-jmjVoFTgNj43aR~fzME6ZZQ~NGjeFO5wX6YZ2ad6Me6deMcA6WJyjfilQ~sLGQ15mabWqTjQMz8~x0xAkDhfIwgzrcjgTwCu39OVb2Obg5dV01ns68Cxh3YL8AvZfbNZH~iidt7minKQpA1R-dw__" alt="" />
+              <span>Zoio <img width='20px' src="https://s3-alpha-sig.figma.com/img/bf46/bf1d/8bd8bcec52422847193f7c361fc0135c?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bNL~YeAL4eGcLhwZYMPrlapxAtjAF1vcAX53yQokun8pV3SNJhwqNXBGOamONbbBQCn1s6eue9rn8OaVm-V8~1pXeKXbP2MVix5XrQmp13p-4UJPPQxWCv83vYRGLcYgQBW0XPy6H4PgLOxaGHQiad7enQVsWz50aE2mZUhMoOrEaQi6xEk3uzVP8ccWL9~m~MyijmprhHX47J~I7l2eJIoBm6Z2EJgx9vXYeXwHvSt-cPQi79NsAL2ndDs6AJmp6T5iTB0BMFVk7jdOO8sKO9HUpYb9CHGuie7jC2x2qrkc5z~O2rkz0DYuyUJxekc434UjEsEocyePLKS7GkM2eg__" alt="" /></span>
+            </div>
+            <p>Procuro alguem para arrumar meu chuveiro.</p>
+            <img src="https://s3-alpha-sig.figma.com/img/ae03/d4a4/950aee4f79c6d77f4e9a65c62a161695?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AvZnV6fl-jcOT6-2yDgw4x0JIaHX2B-gXydFCrt4aw-eMR1bbJl9iIr95J6r042EPfBzMB1a03ZbfSm4dK9oNYlkGQpLGqC-M4GjbHnHZpLjLS14TXHlXU9nTcUin7zsWFtIdZeaDccznBLo48yCbeOZlXtzB7GpsNvcuUNg9cIatQzjyBNsMVinQLPhlaDJBs9~wxQyhwY3Xt1GTo10JKGzjFk-wJX49Q9HskP2ZWOsjZthek~SGOpkqHFc2B4xQTFwk8VDl1J5IS~OyQ8zrW4XwSa6Emp6bUVr0WrnsXAyT12iuYnbjAw9zAN1FfRSFClHCF2dWCE60QrxiPkQfA__" alt="" />
+            <div className='aaaa'>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/8b2b/b269/3b5e68f0db64622933965ab87d7ca17d?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SJuesD4zmm316MJRc6gTdpRQvggSSuAiIzFE~AmKDOIKsSt~CRRBy0JTajINMzQbjizFZzra5s~thh3r10J9-jG6fqB77UcLkLZbGJgIfquqANS8iQMGSrXSHFmX4AT34xAMqLIcobSsNEoH-~6bbICZp85c2GQSeeWc0DQH13e4gA-J1o27H8pQx7wb2rVQ0pUYvjXe6vqXvDjmhq9rUbEXUYDpLQmykhOQAaH2nMNYKSScSrvyMEwfP0qvL-QGAKzaSzTkJ1ows-HecoDQJ~NbC7-mfqpY-a0SkpJbvC958IMlEGsmYXFNX9UN3iHzZUEGzVXAKsE7CJr-BgWcrQ__" alt="" />
+                <span>3</span>
+              </div>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/63af/be91/280e317dcff5a81a9fb9d5cc27d76db3?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MxQw-z2bnuJSR2vAofxxSgoqsMUl~zOP4gSDbpT-imOGrk8PPffg1imf2rXDIZA8cPhAy~xRoO8POtjhpi0OtTGzUBEhgHXcKOKAHYFOFWSIpsF19SukoB9cHnm0Zyoe5LbxhbNrR-28db7L3HQTxVPRsQE~d-EYLZxyyQ6nDQICABoZv0RTqIxOWA~Y1zir4w8jJ7zeb0v~0pfnloAX0378R2qT05C67ZOPSZ2AH11qGc3rmyF24UV1oW2SMZBE9Ngp3zMDP221lUDD9Rhrc3~y88naIQJkFwCqiA6lYgUpfFFoqesVIc59njAxTeyhsGIQwH5kU7ltR-TddonY1g__" alt="" />
+                <span>12</span>
+              </div>
+              <div>
+                <img width="30px" src="https://s3-alpha-sig.figma.com/img/5153/3966/d0b7d0870c425b015fedc9ae4e8b0920?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KWYIVxLXykvknvHVHZFu~2MPLivH833RxYaa13lroXqLalGGRAxsKqF1gUktQA-sOqQWUH~YwPcpRz-OREBxDjisio7JOL0EkfS~RnNOKd4JHMz98kLy0LZW1ToeIs0UyUODBpiK~rK34JRyHDIQZsZtP56FnJ1GppHRPmkriQ9kvMhxtngbhWecogLMQl5rzMjf8mFB6njTw6AnaXT1f1McJWK4YxOGninyllRNVuM2dWa405hu~YmRr8BCbgGuU2zByhcRCAUkjnEd6w1G5w5BM38kLqttFQtFLZJJPRl7riBlXR7D-U8CkSDKxQZWp-4vfDX5IcLJhenHIbmxhw__" alt="" />
+                <span>1</span>
+              </div>
+            </div>
+          </div>
+          <hr />
         </div>
         <div className='cardslaterais'>
           <div>
